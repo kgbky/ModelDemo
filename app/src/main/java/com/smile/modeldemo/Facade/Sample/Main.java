753b72ca15b9694b90fc2876acaf412d51a26a9c
dell <916848592@qq.com>
2019-11-14 13:57:12 +0800

    学习Facade 外观模式

---
+++ app/src/main/java/com/smile/modeldemo/Facade/Sample/Main.java
@@ -0,0 +1,10 @@
+package com.smile.modeldemo.Facade.Sample;
+
+
+import com.smile.modeldemo.Facade.Sample.pagemaker.PageMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        PageMaker.makeWelcomePage("hyuki@hyuki.com", "welcome.html");
+    }
+}
